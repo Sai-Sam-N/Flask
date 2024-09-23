13906e18be17b6a8ea5f75f33edad1c1704412fd
Sai Samyuktha N <73626290+Sai-Sam-N@users.noreply.github.com>
2024-09-23 11:33:51 +0530

    video streaming using webcam

--- opencv_app.py
+++ opencv_app.py
@@ -4,6 +4,29 @@ import cv2
 app=Flask(__name__)
 camera=cv2.VideoCapture(0)
 
+def generate_frames():
+    #Frames should be read continuously
+    while True:
+        #Read the frame
+        success, frame = camera.read() #success-boolean variable
+        if not success:
+            break
+        else:
+            # if we are getting the frame, we will encode it
+            ret, buffer = cv2.imencode('.jpg',frame) #encodes an image into a memory buffer
+            # this buffer can be parsed into an image source
+            # usually when we are parsing from backend to frontend in the form of buffer it will be able to capture and display it in a proper way
+
+            # (2) Convert this buffer back to bytes
+            frame = buffer.tobytes()
+
+            # (3) Return the entire frame - if explicitly done, it captures one or two frames and returns it here
+        # return frame # will just return one frame. But we need other frames, as in the frame has to be returned and keep continuing returning without ending function execution
+        yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') # usually used with a generator; after returning a frame, it would come back, fetch the next frame and return the frame again
+        # whenever images are in the form of bytes, we gotta set it up with Content-Type as specified above
+             
+
 @app.route('/')
 def index():
     return render_template('index_cv2.html')
@@ -11,4 +34,8 @@ def index():
 @app.route('/video')
 def video():
     # Provide the frames we get from the webcam and give it back to the index_cv2.html
-    pass
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame') 
+    #func() takes frames from webcam and pass the entire response to index_cv2.html
+
+if __name__ == '__main__':
+    app.run(debug=True)
